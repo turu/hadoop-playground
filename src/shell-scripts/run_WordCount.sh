9d13cabc5c14758674e6ac588cc5e21b5503ab62
turu <piotr.turu.turek@gmail.com>
2013-07-28 18:52:39 +0200

    Custom config in deployment

--- src/shell-scripts/run_WordCount.sh
+++ src/shell-scripts/run_WordCount.sh
@@ -4,6 +4,11 @@ if [[ -z $HADOOP_HOME ]]; then
     HADOOP_HOME=/opt/hadoop-1.1.2
 fi
 
+#Loading hadoop env settings
+. conf/hadoop-env.sh
+
+#Overriding selected options
+
 job_name=wordcount
 job_executable=lib/hadoop_playground-1.0-SNAPSHOT.jar
 job_main_class=pl.turu.hadoop.mapreduce.wordcount.WordCount
@@ -26,14 +31,13 @@ if [[ $# > 3 ]]; then
     output_hdfs=$4
 fi
 
-pushd $HADOOP_HOME
-bin/hadoop dfs -mkdir $input_hdfs
-bin/hadoop dfs -copyFromLocal $input_local/* $input_hdfs/
-bin/hadoop dfs -mkdir $output_hdfs
+hadoop_exec="$HADOOP_HOME/bin/hadoop --config conf"
+${hadoop_exec} dfs -mkdir $input_hdfs
+${hadoop_exec} -copyFromLocal $input_local/* $input_hdfs/
+${hadoop_exec} -mkdir $output_hdfs
 
-bin/hadoop jar $job_executable $job_main_class $input_hdfs $output_hdfs
+${hadoop_exec} jar $job_executable $job_main_class $input_hdfs $output_hdfs
 
-mkdir $output_local
-bin/hadoop dfs -copyToLocal -ignoreCrc $output_hdfs/* $output_local/
-popd
+#mkdir $output_local
+#bin/hadoop dfs -copyToLocal -ignoreCrc $output_hdfs/* $output_local/
 
